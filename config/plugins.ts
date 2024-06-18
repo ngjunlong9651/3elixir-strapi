42374e893ad9fd2760ae4ab1c0e2522beabd0837
ngjunlong9651 <42053960+ngjunlong9651@users.noreply.github.com>
2024-06-18 15:08:03 +0800

    Increased ratelimit in cofig

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,12 +1,14 @@
-export default ({env}) => ({
-    //...
-    'import-export-entries': {
-      enabled: true,
-      config: {
-        // See `Config` section.
+export default ({ env }) => ({
+  //...
+  "import-export-entries": {
+    enabled: true,
+    config: {
+      ratelimit: {
+        interval: 100000,
+        max: 100000,
       },
+      // See `Config` section.
     },
-    //...
+  },
+  //...
 });
-
-
